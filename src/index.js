f8b8d6b706399c8410d8ab08bece35ffa5f5e7ae
Ganesh Pawar <128301890+ganeshppawar12@users.noreply.github.com>
2023-11-18 18:46:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <BrowserRouter>
+     <HashRouter>
      <Provider store={store}>
 
     <App />
     </Provider>
-    </BrowserRouter>
+    </HashRouter>
 
   </React.StrictMode>
 );
